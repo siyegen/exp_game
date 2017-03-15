bf59d7846a39ccb4e4d3326a79407e6c6b02dcc1
David Tomberlin <siyegen@gmail.com>
2017-03-15 00:43:58 -0700

    initial commit, gots a mesh

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour {
+
+    TileMap currentMap = null;
+    Camera cam = null;
+
+    float camDistance = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        if (cam == null) {
+            Debug.LogError("Couldn't get main camera");
+            return;
+        }
+        Debug.Log("Loading tilemap");
+        TileMap map = Resources.Load<TileMap>("Prefabs/TileMap");
+        Debug.Log(map);
+        currentMap = Instantiate(map) as TileMap;
+
+        Vector3 mapPos = currentMap.transform.position;
+
+        Vector3 camTarget = new Vector3(mapPos.x+0.5f, mapPos.y+camDistance, mapPos.z-0.5f);
+        cam.transform.position = camTarget;
+        cam.transform.Rotate(90, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/TileMap.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class TileMap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Building mesh!");
+        BuildMesh();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    void BuildMesh() {
+
+        Material basicMaterial = Resources.Load<Material>("Materials/Basic");
+
+        Vector3[] vertices = new Vector3[4];
+        int[] triangles = new int[2 * 3];
+        Vector3[] normals = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
+
+        vertices[0] = new Vector3(0, 0, 0);
+        vertices[1] = new Vector3(1, 0, 0);
+        vertices[2] = new Vector3(0, 0, -1);
+        vertices[3] = new Vector3(1, 0, -1);
+
+        triangles[0] = 0;
+        triangles[1] = 3;
+        triangles[2] = 2;
+
+        triangles[3] = 0;
+        triangles[4] = 1;
+        triangles[5] = 3;
+
+        normals[0] = Vector3.up;
+        normals[1] = Vector3.up;
+        normals[2] = Vector3.up;
+        normals[3] = Vector3.up;
+
+        uv[0] = new Vector2(0, 1);
+        uv[1] = new Vector2(1, 1);
+        uv[2] = new Vector2(0, 0);
+        uv[3] = new Vector2(1, 0);
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+
+        meshFilter.mesh = mesh;
+        meshRenderer.sharedMaterial = basicMaterial;
+    }
+}
